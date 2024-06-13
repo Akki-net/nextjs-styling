16e8dd4e1b05947c0d8eb04ef65d501763b790bd
akki@hexerve <akashsharma.zak@gmail.com>
2024-06-13 15:43:07 +0530

    custom styling

---
+++ app/users/List.jsx
@@ -0,0 +1,14 @@
+function List() {
+  return (
+    <>
+    <ul id="userlist">
+        <li>Akash</li>
+        <li>Shivam</li>
+        <li>Rohit</li>
+        <li>Devesh</li>
+    </ul>
+    </>
+  )
+}
+
+export default List
---
+++ app/users/page.js
@@ -0,0 +1,12 @@
+import List from "./List"
+
+function page() {
+  return (
+    <>
+     <h2>This is the user list</h2>
+     <List />
+    </>
+  )
+}
+
+export default page
